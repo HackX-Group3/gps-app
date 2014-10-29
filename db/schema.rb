13e934cccd91aac54c1054fc06a0023ebd2a5dff
yayaya123 <yann.leher@gmail.com>
2014-10-29 14:51:50 +0100

    Scaffold Category + migration + List has_many :categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029134120) do
+ActiveRecord::Schema.define(version: 20141029134955) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "lists", force: true do |t|
     t.string   "name"

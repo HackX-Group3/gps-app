016aa0bdbb65a58c6398060770de8c0633a88169
yayaya123 <yann.leher@gmail.com>
2014-10-29 14:48:17 +0100

    model List et migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029094924) do
+ActiveRecord::Schema.define(version: 20141029134120) do
+
+  create_table "lists", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "lists", ["user_id"], name: "index_lists_on_user_id"
 
   create_table "profiles", force: true do |t|
     t.integer  "user_id"

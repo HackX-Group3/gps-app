891f0cc600f75793d05cd143e011ae8c9b52378a
timmevel <t.mev@hotmail.fr>
2014-12-08 14:39:46 +0100

    gem cors ne marchant pas, modif pour mettre allow ajax request from other domains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  config.middleware.insert_before 0, "Rack::Cors" do
-	allow do
-		origins '*'
-		resource '*', :headers => :any, :methods => [:get, :post, :options, :post]
-	end
-  end
+  before_filter :allow_ajax_request_from_other_domains
 
+	def allow_ajax_request_from_other_domains
+	headers['Access-Control-Allow-Origin'] = '*'
+	headers['Access-Control-Request-Method'] = '*'
+	headers['Access-Control-Allow-Headers'] = '*'
+	end
 end
